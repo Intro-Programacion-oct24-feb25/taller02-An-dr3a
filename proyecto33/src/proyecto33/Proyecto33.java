2814ceb1e0e734c756b504dae9527161557bbb21
Andrea Criollo <criolloandrea20@hotmail.com>
2024-10-17 11:39:20 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto33/src/proyecto33/Proyecto33.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto33;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos basico de Java Versionado a traves de Git Manejado desde GitHub Ademas se usarGitHubClassroom");
+    }
+    
+}
